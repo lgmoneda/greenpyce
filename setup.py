b2d68529fc8875b4b7a893d668bb1580aceccb88
lgmoneda <lg.moneda@gmail.com>
2017-02-27 18:24:28 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='greenpyce',
+      version='0.1',
+      description='Machine Learning Utilities for Pandas',
+      url='http://github.com/lgmoneda/greenpyce',
+      author='Luis Moneda',
+      author_email='lgmoneda@gmail.com',
+      license='MIT',
+      packages=['greenpyce'],
+      zip_safe=False)
